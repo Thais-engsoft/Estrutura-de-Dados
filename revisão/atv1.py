76613898590e9f63f93b19f8285f61eff03e9f7b
Thaís Rodrigues <83671382+Thais-engsoft@users.noreply.github.com>
2023-05-29 16:17:46 -0300

    Update atv1.py
    adicionando comentários no código

--- revisão/atv1.py
+++ revisão/atv1.py
@@ -11,37 +11,59 @@ solicitar 10 entradas, onde será passado a informação do tipo da senha. E a c
 
 
 class Fila:
+    #construção da classe
     def __init__(self):
         self.items = []
 
+    #.append adicionar itens a fila - enfileira
+    #recebe um parâmetro "item", que representa o elemento a ser adicionado à fila - adicionado ao final
     def enfileirar(self, item):
         self.items.append(item)
 
+    #remove e retorna o primeiro elemento da fila. Antes de remover o elemento, é verificado se a fila não está vazia usando o método "esta_vazia". 
+    #Caso a fila não esteja vazia, o método ".pop(0)" é utilizado para remover e retornar o primeiro elemento da lista "items".
     def desenfileirar(self):
         if not self.esta_vazia():
             return self.items.pop(0)
 
+    #Retorna se a lista está ou não vazia
     def esta_vazia(self):
         return len(self.items) == 0
 
+    #retorna a quantidade de elementos da fila
     def tamanho(self):
         return len(self.items)
 
+    #vai mostrar todos os elementos da fila
     def imprimir_fila(self):
         print(self.items)
 
 
+#fila <RECEBE> a classe Fila
 fila = Fila()
+
+#a contagem começa em 0
 contador = 0
 
+#início do loop
 while True:
-    # Solicitar 10 entradas
+
+    # Solicita 10 entradas do usuário
     if contador < 10:
         tipo_senha = int(input("Digite o tipo da senha (1 ou 2): "))
+
+        #vai chamar a classe+método que foi especificado na classe
+        #contador vai adicionando mais um elemento na lista
         fila.enfileirar(tipo_senha)
         contador += 1
+
     else:
         # Realizar atendimento a cada 5 saídas (remoções)
+        #realizar o atendimento de cinco clientes. 
+        #A cada iteração do loop, o método "desenfileirar" da classe "Fila" é chamado para remover o primeiro elemento da fila. 
+        #Se houver um cliente para atender (ou seja, o valor retornado pelo método "desenfileirar" não for "None"), 
+        #é impressa a mensagem "Cliente atendido: Tipo" seguida pelo tipo de senha do cliente atendido. 
+        #Após o loop, o contador é resetado para 0.
         for _ in range(5):
             atendimento = fila.desenfileirar()
             if atendimento is not None:
