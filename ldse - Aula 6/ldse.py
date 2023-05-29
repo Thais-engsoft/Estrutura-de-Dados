2afccce831ea21c641dd1a7f9d09f50fd17cd5db
Thaís Rodrigues <83671382+Thais-engsoft@users.noreply.github.com>
2023-05-29 17:12:49 -0300

    Update ldse.py

--- ldse - Aula 6/ldse.py
+++ ldse - Aula 6/ldse.py
@@ -29,19 +29,36 @@ class ElementList():
      '''implemente os métodos GET e SET para
         se comportarem conforme o comentario
         na descrição do método'''
-     
+    
+    
+    # Retorna o valor correspondente ao índice fornecido
+    # a = instance[5] -- instance.__getitem_(index)
      def __getitem__(self, index):
-        # Retorna o valor correspondente ao índice fornecido
-        # a = instance[5] -- instance.__getitem_(index)
-        
+
+        #Se o índice estiver entre 0 e self.count - 1 (onde self.count representa o número total 
+        #de elementos no objeto), o método retorna o elemento correspondente ao índice usando a 
+        #expressão self.list[index], onde self.list é uma lista (ou outro objeto semelhante) 
+        #contendo os elementos.
         if 0 <= index < self.count:
             return self.list[index]
+        
+
+        #menor que zero ou maior ou igual a self.count, o método lança uma exceção IndexError 
+        #com a mensagem "Índice fora do intervalo válido". Isso indica que o índice fornecido 
+        #não está dentro dos limites permitidos e gera um erro para informar que a operação 
+        #não pode ser realizada.
         else:
             raise IndexError("Índice fora do intervalo válido")
-        
+
+    # Atribui um valor ao índice fornecido
+    # instance[6] = 7   
      def __setitem__(self, index, value):
-        # Atribui um valor ao índice fornecido
-        # instance[6] = 7 
+
+        #value é o valor que será atribuído
+        #Se o índice estiver entre 0 e self.count - 1 (onde self.count representa o número total
+        #de elementos no objeto), o método atribui o valor fornecido ao elemento correspondente 
+        #ao índice usando a expressão self.list[index] = value, onde self.list é uma lista (ou 
+        #outro objeto semelhante) contendo os elementos.
         if 0 <= index < self.count:
             self.list[index] = value
         else:
