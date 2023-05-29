9b4ae323a31c0af434d2d384412fe7d14e08772d
Thaís Rodrigues <83671382+Thais-engsoft@users.noreply.github.com>
2023-05-29 16:37:58 -0300

    Update atv2.py
    Novos comentários para explicação do código

--- revisão/atv2.py
+++ revisão/atv2.py
@@ -1,12 +1,21 @@
 class DuasPilhas:
+
+    #Construção da classe
     def __init__(self):
+        #capacidade total das duas pilhas
         self.capacidade_total = 80
+        #capacidade máxima de cada pilha individualmente
         self.capacidade_pilha = 50
+        #vai armazenar os elementos das pilhas
         self.dados = [None] * self.capacidade_total
+        #presenta o topo das duas pilhas
         self.topo1 = -1
         self.topo2 = self.capacidade_total
 
+    #recebe elemento como parâmetro
+    #verifica se há espaço disponível para empilhar na pilha 1
     def empilhar_pilha1(self, elemento):
+        #se o 1 for menor que o 2, vai incrementar e armazenar em self.dados
         if self.topo1 < self.topo2 - 1:
             self.topo1 += 1
             self.dados[self.topo1] = elemento
@@ -14,13 +23,18 @@ class DuasPilhas:
             print("Pilha 1 está cheia. Não é possível empilhar o elemento.")
 
     def empilhar_pilha2(self, elemento):
+        #Se o 1 for menor que 2, vai decrementar (diminuir valor) 
         if self.topo1 < self.topo2 - 1:
             self.topo2 -= 1
             self.dados[self.topo2] = elemento
         else:
             print("Pilha 2 está cheia. Não é possível empilhar o elemento.")
 
+    #verifica se há elementos na pilha 1
     def desempilhar_pilha1(self):
+        #(se topo1 for maior ou igual a 0). Se houver, o elemento do topo da é retornado (elemento), 
+        #o valor correspondente em dados é definido como None, 
+        # topo1 é decrementado e o elemento é retornado
         if self.topo1 >= 0:
             elemento = self.dados[self.topo1]
             self.dados[self.topo1] = None
@@ -30,6 +44,10 @@ class DuasPilhas:
             print("Pilha 1 está vazia.")
 
     def desempilhar_pilha2(self):
+        #Ele verifica se há elementos na pilha 2 (se topo2 for menor que capacidade_total). 
+        #Se houver, o elemento do topo da pilha é retornado (elemento), o valor correspondente 
+        #em dados é definido como None, topo2 é incrementado e o elemento é retornado. Caso a 
+        #pilha 2 esteja vazia, uma mensagem de erro é exibida.
         if self.topo2 < self.capacidade_total:
             elemento = self.dados[self.topo2]
             self.dados[self.topo2] = None
@@ -39,21 +57,29 @@ class DuasPilhas:
             print("Pilha 2 está vazia.")
 
     def imprimir_pilha1(self):
+        #Ele itera de topo1 até 0 (inclusivo) e imprime o elemento correspondente em dados. 
+        #A cada iteração, um elemento é impresso em uma nova linha. Após imprimir todos os 
+        #elementos, uma linha separadora é exibida.
         print("Pilha 1:")
         for i in range(self.topo1, -1, -1):
             print(self.dados[i])
         print("------------------------")
 
+
     def imprimir_pilha2(self):
+        #Ele itera de topo2 até capacidade_total (exclusivo) e imprime o elemento correspondente
+        #em dados. A cada iteração, um elemento é impresso em uma nova linha. 
+        #Após imprimir todos os elementos, uma linha separadora é exibida.
         print("Pilha 2:")
         for i in range(self.topo2, self.capacidade_total):
             print(self.dados[i])
         print("------------------------")
 
 
-# Exemplo de uso
+#pilhas vai receber os métodos da classe DuasPilhas
 pilhas = DuasPilhas()
 
+#empilha o elemento x na pilha x
 pilhas.empilhar_pilha1(10)
 pilhas.empilhar_pilha1(20)
 pilhas.empilhar_pilha1(30)
@@ -62,10 +88,13 @@ pilhas.empilhar_pilha2(50)
 pilhas.imprimir_pilha1()
 pilhas.imprimir_pilha2()
 
+#desempilha elemento x da pilha x
 elemento1 = pilhas.desempilhar_pilha1()
 elemento2 = pilhas.desempilhar_pilha2()
 print("Elemento desempilhado da Pilha 1:", elemento1)
 print("Elemento desempilhado da Pilha 2:", elemento2)
+print("------------------------")
 
+#imprime os elemento da pilha x
 pilhas.imprimir_pilha1()
 pilhas.imprimir_pilha2()
